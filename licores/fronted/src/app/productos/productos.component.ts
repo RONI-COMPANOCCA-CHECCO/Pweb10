5119d0efd82af15538a93853bec9e3d91d5fa72c
rcompanocca <ronicompanocca@gmail.com>
2024-07-04 23:27:00 -0500

    usamos el servicio en los componentes

--- licores/fronted/src/app/productos/productos.component.ts
+++ licores/fronted/src/app/productos/productos.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventarioService } from '../services/inventario.service';
 
 @Component({
   selector: 'app-productos',
-  standalone: true,
-  imports: [],
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.css'
+  styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  productos: any = [];
 
+  constructor(private inventarioService: InventarioService) { }
+
+  ngOnInit(): void {
+    this.inventarioService.getProductos().then(response => {
+      this.productos = response.data;
+    }).catch(error => {
+      console.error('Error fetching productos:', error);
+    });
+  }
 }
